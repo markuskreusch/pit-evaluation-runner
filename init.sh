abd9662dca9cb498a404b1698b9779fb5d2c2df5
Markus kreusch <mail@markuskreusch.de>
2023-02-26 15:14:07 +0000

    Fixed repo setup

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+set -e
+
 git clone https://github.com/markuskreusch/pitest.git
 git clone https://github.com/markuskreusch/commons-lang.git
+cd pitest
+git checkout meta_mutants
+mvn clean install
