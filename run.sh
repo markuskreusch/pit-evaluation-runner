34c64a7779fed452ba11b1ec880c2627c7459843
Markus kreusch <mail@markuskreusch.de>
2023-02-26 15:55:12 +0000

    Updated run script

--- run.sh
+++ run.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 set -xe
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 function runAnalysis() {
-	version=$1
-	out=../pit-commons-lang-$version.out
-	git checkout pit/$version 2>&1 >$out
-	mvn -e clean install 2>&1 >$out
-	date >$out
-	mvn -e pitest:mutationCoverage 2>&1 >$out
-	date >$out
+	project=$1
+	version=$2
+	out=$SCRIPT_DIR/pit.$project.$version.out
+	pushd $project
+	git checkout pit/$version 2>&1 >>"$out"
+	mvn -e clean install 2>&1 >>"$out"
+	date >>"$out"
+	mvn -e pitest:mutationCoverage 2>&1 >>"$out"
+	date >>"$out"
+	popd
 }
 
-cd commons-lang
+# remove old output files
+cd "$SCRIPT_DIR"
+rm -f pit.*.out
 
-runAnalysis 3.9
-runAnalysis 3.10
-runAnalysis 3.11
-runAnalysis 3.12
+# run analysis
+runAnalysis commons-lang 3.9
+runAnalysis commons-lang 3.10
+runAnalysis commons-lang 3.11
+runAnalysis commons-lang 3.12
