6fbf48b0e1f23f246ecbfb95369989990f2beef4
Markus kreusch <mail@markuskreusch.de>
2023-03-04 19:01:07 +0000

    Running both, regular and optimized analysis

--- run.sh
+++ run.sh
@@ -6,12 +6,19 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 function runAnalysis() {
 	project=$1
 	version=$2
-	out=$SCRIPT_DIR/pit.$project.$version.out
+	index=$3
+	typ=$4
+	out=$SCRIPT_DIR/pit.$project.$version.$typ.$index.out
+	if [ "$typ" = "metaMutationAnalysis" ]; then
+		metaMutationAnalysis=true
+	else
+		metaMutationAnalysis=false
+	fi
 	pushd $project
-	git checkout pit/$version 2>&1 >>"$out"
-	mvn -e clean install 2>&1 >>"$out"
+	git checkout pit/$version >>"$out" 2>&1
+	mvn -e clean install >>"$out" 2>&1
 	date >>"$out"
-	mvn -e pitest:mutationCoverage 2>&1 >>"$out"
+	mvn -e pitest:mutationCoverage -Dpit.metaMutationAnalysis=$metaMutationAnalysis >>"$out" 2>&1
 	date >>"$out"
 	popd
 }
@@ -21,7 +28,26 @@ cd "$SCRIPT_DIR"
 rm -f pit.*.out
 
 # run analysis
-runAnalysis commons-lang 3.9
-runAnalysis commons-lang 3.10
-runAnalysis commons-lang 3.11
-runAnalysis commons-lang 3.12
+
+
+for i in $(seq 1 1);
+do
+	echo "Init run #$i"
+	date
+	runAnalysis commons-lang 3.9 $i init
+	date
+
+	echo "Normal run #$i"
+	date
+	runAnalysis commons-lang 3.10 $i normal
+	runAnalysis commons-lang 3.11 $i normal
+	runAnalysis commons-lang 3.12 $i normal
+	date
+
+	echo "Meta Mutation Analysis Optimized run #$i"
+	date
+	runAnalysis commons-lang 3.10 $i metaMutationAnalysis
+	runAnalysis commons-lang 3.11 $i metaMutationAnalysis
+	runAnalysis commons-lang 3.12 $i metaMutationAnalysis
+	date
+done
