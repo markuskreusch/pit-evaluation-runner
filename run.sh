2bbdffb9ca31eb39afbc9897fe2c786e638a2773
Markus kreusch <mail@markuskreusch.de>
2023-03-08 16:52:25 +0000

    Changed run.sh and added script to extract results

--- run.sh
+++ run.sh
@@ -9,16 +9,25 @@ function runAnalysis() {
 	index=$3
 	typ=$4
 	out=$SCRIPT_DIR/pit.$project.$version.$typ.$index.out
-	if [ "$typ" = "metaMutationAnalysis" ]; then
+	metaMutationUnitSize=1
+	if [ "$typ" = "metaMutationAnalysisSingle" ]; then
 		metaMutationAnalysis=true
+	elif [ "$typ" = "metaMutationAnalysisGroup" ]; then
+		metaMutatonAnalysis=true
+		metaMutationUnitSize=999999
 	else
 		metaMutationAnalysis=false
+	fi
+	if [ "$typ" = "prioritiseKillingTests" ]; then
+		prioritiseKillingTests=true
+	else
+		prioritiseKillingTests=false
 	fi
 	pushd $project
 	git checkout pit/$version >>"$out" 2>&1
 	mvn -e clean install >>"$out" 2>&1
 	date >>"$out"
-	mvn -e pitest:mutationCoverage -Dpit.metaMutationAnalysis=$metaMutationAnalysis >>"$out" 2>&1
+	mvn -e pitest:mutationCoverage -Dpit.metaMutationAnalysis=$metaMutationAnalysis -Dpit.metaMutationUnitSize=$metaMutationUnitSize -Dpit.prioritiseKillingTests=$prioritiseKillingTests -Dpit.verbose=false >>"$out" 2>&1
 	date >>"$out"
 	popd
 }
@@ -30,13 +39,13 @@ rm -f pit.*.out
 # run analysis
 
 
+echo "Init run"
+date
+runAnalysis commons-lang 3.9 0 init
+date
+
 for i in $(seq 1 100);
 do
-	echo "Init run #$i"
-	date
-	runAnalysis commons-lang 3.9 $i init
-	date
-
 	echo "Normal run #$i"
 	date
 	runAnalysis commons-lang 3.10 $i normal
@@ -44,10 +53,24 @@ do
 	runAnalysis commons-lang 3.12 $i normal
 	date
 
-	echo "Meta Mutation Analysis Optimized run #$i"
+	echo "Meta Mutation Analysis Single Optimized run #$i"
 	date
-	runAnalysis commons-lang 3.10 $i metaMutationAnalysis
-	runAnalysis commons-lang 3.11 $i metaMutationAnalysis
-	runAnalysis commons-lang 3.12 $i metaMutationAnalysis
+	runAnalysis commons-lang 3.10 $i metaMutationAnalysisSingle
+	runAnalysis commons-lang 3.11 $i metaMutationAnalysisSingle
+	runAnalysis commons-lang 3.12 $i metaMutationAnalysisSingle
+	date
+
+	echo "Meta Mutation Analysis Group Optimized run #$i"
+	date
+	runAnalysis commons-lang 3.10 $i metaMutationAnalysisGroup
+	runAnalysis commons-lang 3.11 $i metaMutationAnalysisGroup
+	runAnalysis commons-lang 3.12 $i metaMutationAnalysisGroup
+	date
+
+	echo "Killing Test Priority Optimized run #$i"
+	date
+	runAnalysis commons-lang 3.10 $i prioritiseKillingTests
+	runAnalysis commons-lang 3.11 $i prioritiseKillingTests
+	runAnalysis commons-lang 3.12 $i prioritiseKillingTests
 	date
 done
